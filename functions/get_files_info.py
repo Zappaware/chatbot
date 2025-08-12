1892d7d3895b22767c1d50effc8261aa50ac8745
Zappaware <sergio_germanc@hotmail.com>
2025-08-12 14:26:22 -0600

    Add hability to take functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -29,6 +30,20 @@ def get_files_info(working_directory, directory="."):
     except Exception as e:
         # Catch any error and return it as a string with "Error:" prefix
         return f"Error: {str(e)}"
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
     
     
     
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -10,7 +11,24 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    system_prompt = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+    model_name = "gemini-2.0-flash-001"
+
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+    
 
     if len(sys.argv) > 1:
         user_prompt = sys.argv[1]
@@ -24,10 +42,20 @@ def main():
 
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
-    )
+    model=model_name,
+    contents=messages,
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+   ),
+)
+
+    if response.function_calls:
+        function_call_part = response.function_calls[0]
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
     print(response.text)
+
+    
 
     if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
         print(f"User prompt: {user_prompt}")
