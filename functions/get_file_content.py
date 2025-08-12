0e1d8d4713bb3f05eaba60e065c4556f2b683308
Zappaware <sergio_germanc@hotmail.com>
2025-08-12 12:43:27 -0600

    Modify return strings from get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,19 +1,19 @@
 import os
 
-def get_file_content(working_directory, directory):
+def get_file_content(working_directory, file_path):
     try:
         # Normalize working directory and target directory
         working_directory = os.path.abspath(working_directory)
-        target_path = os.path.abspath(os.path.join(working_directory, directory))
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
 
         # Ensure target is within working_directory
         if not target_path.startswith(working_directory):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
 
         # Ensure target is actually a directory
         if not os.path.isfile(target_path):
-            return f'Error: "{directory}" is not a directory'
-        
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
         MAX_CHARS = 10000
 
         with open(target_path, "r") as f:
