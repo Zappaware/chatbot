9532e8f17ce31a2dc4eea527c8fcecd4aba82654
Zappaware <sergio_germanc@hotmail.com>
2025-08-12 13:03:59 -0600

    Add functionality of writing a file

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+         # Normalize working directory and target directory
+        working_directory = os.path.abspath(working_directory)
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        # Ensure target is within working_directory
+        if not target_path.startswith(working_directory):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        # Ensure target exists
+        if not os.path.exists(target_path):
+            #create file_path
+            os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+        with open(target_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        # Catch any error and return it as a string with "Error:" prefix
+        return f"Error: {str(e)}"
